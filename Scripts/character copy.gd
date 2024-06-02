1d26a4f2a846fe547e2ad235fa96312d6d09c769
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-06-03 00:04:11 +0300

    added a mechanic to the ball that it stops when i release movement keys, also adjusted my camera settings to follow the ball whenever it goes

--- Scripts/character copy.gd
+++ Scripts/character copy.gd
@@ -9,16 +9,17 @@ func _ready():
 func _physics_process(delta):
 	# this function is the main function for the game it contans the main controls and movement mechanics that we will
 	# be needing for this game
-	velocity = Vector3.ZERO
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = stop
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = charSpeed
+		velocity.z = stop
 		$MeshInstance3D.rotate_y(deg_to_rad(8))
 
 		print("right arrow pressed and velocity is adjusted to ", velocity.x)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -charSpeed
+		velocity.z = stop
 		$MeshInstance3D.rotate_y(deg_to_rad(-8))
 
 		print("left arrow pressed and velocity is adjusted to ", velocity.x)
@@ -29,15 +30,17 @@ func _physics_process(delta):
 		velocity.z = stop
 	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -charSpeed
+		velocity.x = stop
 		$MeshInstance3D.rotate_x(deg_to_rad(-8))
 
 		print("up arrow pressed and velocity is adjusted to ", velocity.z)
 	elif Input.is_action_pressed("ui_down"):
 		velocity.z = charSpeed
+		velocity.x = stop
 		$MeshInstance3D.rotate_x(deg_to_rad(8))
 
 		print("down arrow pressed and velocity is adjusted to ", velocity.z)
-	lerp(velocity.z, 0.0, 0.1)
-	lerp(velocity.x, 0.0, 0.1)
+	else:
+		lerp(velocity.z, 0.0, 0.1)
 
-	move_and_collide(velocity * delta)
+	move_and_slide()
