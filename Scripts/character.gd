ff49e15af6eb7f463ddd5c1e9e51436ceb694bdb
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-06-02 21:49:41 +0300

    added rotation mechanic to the mesh of the ball

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -14,10 +14,14 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = charSpeed
 		velocity.z = stop
+		$MeshInstance3D.rotate_y(deg_to_rad(8))
+
 		print("right arrow pressed and velocity is adjusted to ", velocity.x)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -charSpeed
 		velocity.z = stop
+		$MeshInstance3D.rotate_y(deg_to_rad(-8))
+
 		print("left arrow pressed and velocity is adjusted to ", velocity.x)
 	else:
 		lerp(velocity.x, 0.0, 0.1)
@@ -27,10 +31,14 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -charSpeed
 		velocity.x = stop
+		$MeshInstance3D.rotate_x(deg_to_rad(-8))
+
 		print("up arrow pressed and velocity is adjusted to ", velocity.z)
 	elif Input.is_action_pressed("ui_down"):
 		velocity.z = charSpeed
 		velocity.x = stop
+		$MeshInstance3D.rotate_x(deg_to_rad(8))
+
 		print("down arrow pressed and velocity is adjusted to ", velocity.z)
 	else:
 		lerp(velocity.z, 0.0, 0.1)
